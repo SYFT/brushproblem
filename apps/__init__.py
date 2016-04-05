dc015a31b3bfdab63652de1412aa2320d4e7666e
SYFT <smallrqnoj@163.com>
2016-04-05 14:35:11 +0800

    0405 database

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 #app.secret_key = '123456'
 app.config.from_object('config')
-from apps import views
+db = SQLAlchemy(app)
+from apps import views, models
--- apps/views.py
+++ apps/views.py
@@ -1,27 +1,26 @@
 from apps import app
 from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash
+	 abort, render_template, flash
+from .forms import LoginForm
 
 @app.route('/')
-@app.route('/index/<status>')
-def index(status = False):
+@app.route('/index/')
+def index():
 	#user = {'username' : 'admin'}
-	return render_template('index.html', userstatus = status)
+	return render_template('index.html', userstatus = False)
 	#return "Hello World"
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or \
-           request.form['password'] != 'secret':
-            flash(u'Invalid username or assword')
-            error = 'Invalid credentials'
-        else:
-            flash('You were successfully logged in')
-            flash('Welcome back %s' % request.form['username'])
-            return redirect(url_for('index', status = True))
-    return render_template('login.html', error=error)
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('OpenID: ' + form.openid.data + '\n')
+		flash('remember_me : ' + str(form.remember_me.data) + '\n')
+		return redirect('/index')
+	return render_template('login.html', 
+							title = 'Sign In', 
+							form = form,
+							providers = app.config['OPENID_PROVIDERS'])
 
 @app.route('/details')
 def details():
